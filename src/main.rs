88a70f65748a2ddb2de20f4ce1ca39daf02a3b5b
egsam98 <egsam98@gmail.com>
2020-04-03 19:03:31 +0300

    API documentation

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 
+use rocket_contrib::serve::StaticFiles;
 use crate::db::{SqlitePool};
 use std::panic;
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
 
 #[macro_use]mod utils;
 mod db;
@@ -17,6 +20,7 @@ mod controllers;
 mod schema;
 
 const LOG_CONFIG_FILENAME: &str = "log4rs.yml";
+const STATIC_PATH_PREFIX: &str = "static";
 
 fn configure_logger() {
     log4rs::init_file(LOG_CONFIG_FILENAME, Default::default()).unwrap();
@@ -25,10 +29,19 @@ fn configure_logger() {
     }));
 }
 
+#[get("/")]
+fn docs() -> Template {
+    Template::render("index", HashMap::<String, String>::new())
+}
+
 fn main() {
     configure_logger();
     rocket::ignite()
         .manage(SqlitePool::new())
+        .mount("/", routes!(docs))
+        .mount("/docs", routes!(docs))
+        .mount("/public", StaticFiles::from(STATIC_PATH_PREFIX))
         .mount("/api/image", controllers::init_routes())
+        .attach(Template::fairing())
         .launch();
 }
